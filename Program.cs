95d1226508d177d9e37d6e1fcd5c0d782189a87a
陈敏华 <chenminhua@SAW-L7607764F3G.guest.corp.microsoft.com>
2020-05-13 17:27:05 +0800

    xml

--- Program.cs
+++ Program.cs
@@ -1,7 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-
-
+using System.Xml.Linq;
+using System.IO;
+using System.Linq;
 
 namespace GlossaryNS
 {
@@ -37,15 +38,48 @@ namespace GlossaryNS
     {
         static void Main(string[] args)
         {
-            if (args.Length < 2) {
-                throw new System.ArgumentException("try these: \n" +  
-                                "    glossary add srp='storage resource provider' \n" + 
+            // args
+            if (args.Length < 2)
+            {
+                throw new System.ArgumentException("try these: \n" +
+                                "    glossary add srp='storage resource provider' \n" +
                                 "  or \n" +
                                 "    glossary query srp");
             }
             Console.WriteLine(args[0]);
             Console.WriteLine(args[1]);
+
+            // xml
+            var filename = "glossary.xml";
+            var currentDirectory = Directory.GetCurrentDirectory();
+            var purchaseOrderFilepath = Path.Combine(currentDirectory, filename);
+
+            // no this file, create
+            if (true)
+            {
+                XElement contacts =
+                    new XElement("Contacts",
+                        new XElement("Contact",
+                            new XElement("Name", "Patrick Hines"),
+                            new XElement("Phone", "206-555-0144",
+                                new XAttribute("Type", "Home")),
+                            new XElement("phone", "425-555-0145",
+                                new XAttribute("Type", "Work")),
+                            new XElement("Address",
+                                new XElement("Street1", "123 Main St"),
+                                new XElement("City", "Mercer Island"),
+                                new XElement("State", "WA"),
+                                new XElement("Postal", "68042")
+                            )
+                        )
+                    );
+            }
+
+            //XElement purchaseOrder = XElement.Load(purchaseOrderFilepath);
+
+            // IEnumerable<string> partNos = from item in purchaseOrder.Descendants("Item")
+            //                               select (string)item.Attribute("PartNumber");
         }
-        
+
     }
 }
